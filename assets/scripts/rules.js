c315ceefcf33e5a48888552c8d3e5c1355147193
Vinicius <79338898+vinipet@users.noreply.github.com>
2024-01-05 17:16:07 -0300

    bug fix

--- assets/scripts/rules.js
+++ assets/scripts/rules.js
@@ -5,14 +5,8 @@ export default function createGame(){
          'player1': {x:0, y:12}
       },
       walls: {
-         'wall1': {x:0, y:0, height: 1, width:25},
-         'wall2': {x:0, y:0, height: 12, width:1},
-         'wall3': {x:0, y:13, height: 12, width:1},
-         'wall4': {x:24, y:0, height: 12, width:1},
-         'wall5': {x:24, y:13, height: 12, width:1},
-         'wall6': {x:0, y:24, height: 1, width:25},
-         'wall7': {x:0, y:13, height: 1, width:10},
-         'wall8': {x:0, y:11, height: 1, width:10},
+         'wall1': {x:1, y:12,},
+         'wall2': {x:3, y:12}
       },
       destiny: {
          '1': {x:24, y:12, height:1, width:1}
@@ -22,22 +16,22 @@ export default function createGame(){
 
       const acceptedMovements = {
          ArrowUp(player){
-            if(player.y-1 >=0){
+            if(player.y-1 >=0 && wall(0,-1) && win(0,-1)){
                player.y = player.y-1
             }
          },
          ArrowRight(player){
-            if(player.x+1<state.screen.width){
+            if(player.x+1<state.screen.width && wall(+1,0) && win(+1,0)){
                player.x =player.x+1
             }
          },
          ArrowDown(player){
-            if(player.y+1<state.screen.height){
+            if(player.y+1<state.screen.height && wall(0,+1) && win(0,+1)){
                player.y=player.y+1
             }
          },
          ArrowLeft(player){
-            if(player.x-1>=0){
+            if(player.x-1>=0 && wall(-1,0) && win(-1,0)){
                player.x=player.x-1
             }
          },
@@ -59,8 +53,28 @@ export default function createGame(){
    }
 }
 
-function wall(side){
-   for(const wall in game.state.walls){
-      return walls.side
+function wall(commandX, commandY){
+   const playerPos = state.players.player1
+   
+   for(let sla in state.walls){
+      let wall = state.walls[sla]
+      if((playerPos.x + commandX) == wall.x && (playerPos.y + commandY)== wall.y){ 
+         return false
+      } else{
+         return true
+      }
+   }
+}
+
+function win(commandX, commandY){
+   const playerPos = state.players.player1
+   
+   for(let sla in state.destiny){
+      let end = state.destiny[sla]
+      if((playerPos.x + commandX) == end.x && (playerPos.y + commandY)== end.y){ 
+         window.alert('foi')
+      } else{
+         return true
+      }
    }
 }
