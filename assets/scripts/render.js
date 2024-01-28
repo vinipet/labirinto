924c6df1f9d09ca44eb75da06bfcf9f700138d82
Vinicius <79338898+vinipet@users.noreply.github.com>
2024-01-28 10:05:40 -0300

    Update render.js

--- assets/scripts/render.js
+++ assets/scripts/render.js
@@ -6,18 +6,11 @@ export default function renderScreen(screen, game, requestAnimationFrame) {
    screen.fillStyle = 'white'
    screen.clearRect(0,0,game.state.screen.width,game.state.screen.height)
    
-   for(const playerId in game.state.players){
-      const player = game.state.players[playerId]
-      screen.fillStyle = 'blue'
-      screen.fillRect(player.x, player.y, 1, 1)
-   }
-
    for(const wallId in game.state.walls){
       const wall = game.state.walls[wallId]
       screen.fillStyle = 'white'
       screen.fillRect(wall.x, wall.y, 1, 1)
    }
-   
    for(const destinyId in game.state.destiny){
       const destiny = game.state.destiny[destinyId]
       screen.fillStyle = 'green'
@@ -32,6 +25,11 @@ export default function renderScreen(screen, game, requestAnimationFrame) {
       const tp = game.state.tp[tpId]
       screen.fillStyle = 'brown'
       screen.fillRect(tp.x, tp.y,1,1)
+   }
+   for(const playerId in game.state.players){
+      const player = game.state.players[playerId]
+      screen.fillStyle = 'blue'
+      screen.fillRect(player.x, player.y, 1, 1)
    }
    requestAnimationFrame(() => {
       renderScreen(screen, game, requestAnimationFrame)
