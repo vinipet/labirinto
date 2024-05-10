6d0358231873eb8ea6bf307f76ed3b66edafa7ad
Vinipet <79338898+vinipet@users.noreply.github.com>
2024-05-10 10:22:33 -0300

    name changed

--- back-end/server.js
+++ back-end/server.js
@@ -2,46 +2,60 @@ import Express  from "express";
 import http from 'http'
 import CreateGame from '../public/assets/scripts/rules.js'
 import {Server} from 'socket.io'
+import mundo1 from '../back-end/worldsMaps/wolrd1.js'
+import path from 'path'
 
+
+
+const __dirname = path.resolve()
 const app = Express()
 const server = http.createServer(app)
 const sockets = new Server(server)
+
 app.use(Express.static('public'))
 
+const mundo = []
+mundo.push(mundo1)
 
+app.post('/play', function(req, res){
 
+   console.log(req.se)
 
-import mundo from '../back-end/worldsMaps/wolrd2.js'
-const game = CreateGame()
-game.setState(mundo)
-console.log(game.state)
+   
+   res.sendFile(__dirname + '/public/play.html')
+   const game = CreateGame()
+   game.setState(mundo[0])
+   // console.log(game.state)
+   
+   game.subscribe((command)=>{
+      // console.log(`> emitting ${command.type}`)
+      sockets.emit(command.type, command)
+   })
+   
+   
+   sockets.on( 'connection', (socket)=>{
+      const playerId = socket.id
+      // console.log(`> player conected on id ${playerId}`)
+   
+      game.addPlayer({playerId:playerId, playerX:0,playerY:0})
+      socket.emit('setup', game.state)
+   
+      socket.on('disconnect', () => {
 
-game.subscribe((command)=>{
-   console.log(`> emitting ${command.type}`)
-   sockets.emit(command.type, command)
+         game.removePlayer({playerId:playerId})
+      })
+   
+      socket.on('move-player', (command)=>{
+         command.playerId = playerId
+         command.type = 'move-player'
+         game.movePlayer(command)
+      })
+   
+   })
+   
 })
 
 
-sockets.on( 'connection', (socket)=>{
-   const playerId = socket.id
-   console.log(`> player conected on id ${playerId}`)
-
-   game.addPlayer({playerId:playerId, playerX:0,playerY:0})
-   console.log(game.state)
-
-   socket.emit('setup', game.state)
-
-   socket.on('disconnect', () => {
-      game.removePlayer({playerId:playerId})
-   })
-
-   socket.on('move-player', (command)=>{
-      command.playerId = playerId
-      command.type = 'move-player'
-      game.movePlayer(command)
-   })
-
-})
 
 
 
